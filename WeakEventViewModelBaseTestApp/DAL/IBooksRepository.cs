8ccc9df0fb08b2d9e25a2d9909b7b08459127985
MakMokMak <mak@makcraft.com>
2020-01-23 21:41:13 +0900

    Add project. - WeakEventViewModelBaseTestApp

---
+++ WeakEventViewModelBaseTestApp/DAL/IBooksRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using WeakEventViewModelBaseTestApp.Models;
+
+namespace WeakEventViewModelBaseTestApp.DAL
+{
+    interface IBooksRepository
+    {
+        int Count { get; }
+
+        IQueryable<Book> FindBooks();
+        Book GetBook(int id);
+
+        Book Add(Book book);
+        void Update(Book book);
+        void Delete(int id);
+    }
+}
---
+++ WeakEventViewModelBaseTestApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using MakCraft.ViewModels;
+using WeakEventViewModelBaseTestApp.Models;
+using WeakEventViewModelBaseTestApp.Services;
+
+namespace WeakEventViewModelBaseTestApp.ViewModels
+{
+    class MainWindowViewModel : WeakEventViewModelBase
+    {
+        private readonly IBookService _service;
+        private readonly IPropertyChangedWeakEventListener _listener;
+
+        public MainWindowViewModel() : this(new BookService(), new PropertyChangedWeakEventListener()) { }
+        public MainWindowViewModel(IBookService service, IPropertyChangedWeakEventListener listener)
+        {
+            _service = service;
+            _listener = listener;
+            _listener.WeakPropertyChanged += OnWeakListenerPropertyChanged;
+            base.AddListener(_service, _listener);
+        }
+
+        public int BooksCount
+        {
+            get { return _service.Count; }
+        }
+
+        public List<Book> Books
+        {
+            get { return _service.Books; }
+        }
+
+        private void AddBookCommandExecute()
+        {
+            var book = new Book
+            {
+                Id = 0,
+                Title = "くまの本",
+                Author = "森のくまさん",
+                Price = 1600,
+                Feature = "森のくまさんの生活を描いた自信作"
+            };
+            _service.AddBook(book);
+        }
+        private ICommand _addBookCommand;
+        public ICommand AddBookCommand
+        {
+            get
+            {
+                if (_addBookCommand == null)
+                    _addBookCommand = new RelayCommand(AddBookCommandExecute);
+                return _addBookCommand;
+            }
+        }
+
+        private void OnWeakListenerPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.RaisePropertyChanged(nameof(BooksCount));
+            base.RaisePropertyChanged(nameof(Books));
+        }
+    }
+}
